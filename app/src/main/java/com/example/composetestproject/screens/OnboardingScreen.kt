a192ee62269fe47d0c0e1d4ddf5feebb9fa309eb
aderemilekun <adelefatai@yahoo.com>
2023-04-02 14:32:42 +0100

    Completed the login bottom sheet

--- app/src/main/java/com/example/composetestproject/screens/OnboardingScreen.kt
+++ app/src/main/java/com/example/composetestproject/screens/OnboardingScreen.kt
@@ -1,25 +1,34 @@
 package com.example.composetestproject.screens
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.composetestproject.R
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -34,21 +43,115 @@ fun OnboardingScreen(navController: NavController) {
         skipHalfExpanded = true
     )
 
+    val email = rememberSaveable {
+        mutableStateOf("")
+    }
+
+    var loading: Boolean = false
+
+    val password = rememberSaveable {
+        mutableStateOf("")
+    }
+
+    val passwordVisibility = rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    val passwordFocusRequest = FocusRequester.Default
+
+    val valid = remember(email.value, password.value) {
+        email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
+    }
+
     ModalBottomSheetLayout(
         sheetState = modalSheetState,
         sheetShape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp),
-
-
+        sheetBackgroundColor = Color(0xff222232),
         sheetContent = {
             Column(
-                //...
+               horizontalAlignment = Alignment.Start,
+
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxHeight(0.57f)
             ) {
+                Spacer(modifier = Modifier.height(15.dp))
+
+                Text(
+                    text = "Welcome",
+//                letterSpacing = letterSpacing,
+                    style = MaterialTheme.typography.h5.copy(
+                        color = Color.White,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                EmailInput(
+                    emailState = email,
+                    enabled = !loading,
+                    leadingIcon = painterResource(id = R.drawable.message),
+                    onAction = KeyboardActions {
+//                        passwordFocusRequest.requestFocus()
+                    })
+                PasswordInput(
+                    modifier = Modifier.focusRequester(passwordFocusRequest),
+                    passwordState = password,
+                    labelId = "Password",
+                    enabled = !loading,
+                    passwordVisibility = passwordVisibility,
+                    leadingIcon= painterResource(id = R.drawable.password),
+                    onAction = KeyboardActions {
+
+                        if (!valid) return@KeyboardActions
+//                        onDone(email.value.trim(), password.value.trim())
+                    }) {
+
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 20.dp, bottom = 20.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+
+                    ) {
+                        Checkbox(
+                            checked = false,
+                            onCheckedChange = {},
+                            colors = CheckboxDefaults.colors(uncheckedColor = Color(0xff65656B))
+                        )
+                        Text(text = "Remember me", color = Color(0xff65656B))
+                    }
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Forgot Password", color = Color.White)
+                    }
+                }
                 Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(60.dp)
+                        .clip(RoundedCornerShape(15.dp)),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xff246BFD)),
                     onClick = {
                         coroutineScope.launch { modalSheetState.hide() }
                     }
                 ) {
-                    Text(text = "Hide Sheet")
+                    Text(text = "Sign in", color = Color.White)
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(text = "Don't have an account?", color = Color.White)
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Sign Up", color = Color(0xff246BFD))
+                    }
                 }
             }
         }
@@ -124,4 +227,135 @@ fun OnboardingScreen(navController: NavController) {
 //        }
     }
 
+}
+
+@Composable
+fun EmailInput(
+    modifier: Modifier = Modifier,
+    emailState: MutableState<String>,
+    labelId: String = "Email",
+    enabled: Boolean = true,
+    imeAction: ImeAction = ImeAction.Next,
+    leadingIcon: Painter,
+    onAction: KeyboardActions = KeyboardActions.Default
+) {
+
+    InputField(
+        modifier = modifier,
+        valueState = emailState,
+        labelId = labelId,
+        enabled = enabled,
+        keyboardType = KeyboardType.Email,
+        imeAction = imeAction,
+        leadingIcon = leadingIcon,
+        onAction = onAction
+    )
+}
+
+@Composable
+fun InputField(
+    modifier: Modifier = Modifier,
+    valueState: MutableState<String>,
+    labelId: String,
+    enabled: Boolean,
+    isSingleLine: Boolean = true,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Next,
+    leadingIcon: Painter,
+//    trailingIcon: Painter,
+    onAction: KeyboardActions = KeyboardActions.Default
+) {
+    OutlinedTextField(
+        value = valueState.value,
+        onValueChange = { valueState.value = it },
+        label = {
+            Text(
+                text = labelId, color = Color(0xff65656B)
+            )
+        },
+        singleLine = isSingleLine,
+        textStyle = TextStyle(
+            fontSize = 18.sp,
+            color = Color(0xff65656B)
+        ),
+        modifier = modifier
+            .padding(bottom = 10.dp)
+            .fillMaxWidth(),
+        enabled = enabled,
+        shape = RoundedCornerShape(15.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction),
+        keyboardActions = onAction,
+        leadingIcon = {
+            Icon(
+                painter = leadingIcon,
+                contentDescription = "TextField Leading Icon",
+                modifier = Modifier.size(25.dp)
+            )
+        },
+        //#65656B
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            unfocusedBorderColor = Color(0xff222232),
+            backgroundColor = Color(0xff181829),
+            leadingIconColor = Color(0xff65656B)
+        )
+    )
+}
+
+@Composable
+fun PasswordInput(
+    modifier: Modifier,
+    passwordState: MutableState<String>,
+    labelId: String,
+    enabled: Boolean,
+    passwordVisibility: MutableState<Boolean>,
+    onAction: KeyboardActions = KeyboardActions.Default,
+    imeAction: ImeAction = ImeAction.Done,
+    leadingIcon: Painter,
+    function: () -> Unit
+) {
+    val visualTransformation = if (passwordVisibility.value) VisualTransformation.None else PasswordVisualTransformation()
+    OutlinedTextField(
+        value = passwordState.value,
+        onValueChange = {
+            passwordState.value = it
+        },
+        label = { Text(
+            text = labelId, color = Color(0xff65656B)
+        ) },
+        singleLine = true,
+        textStyle = TextStyle(fontSize = 18.sp, color = Color(0xff65656B)),
+        modifier = modifier
+            .padding(bottom = 10.dp)
+            .fillMaxWidth(),
+        enabled = enabled,
+        shape = RoundedCornerShape(15.dp),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password,
+            imeAction = imeAction
+        ), visualTransformation = visualTransformation,
+        leadingIcon = {
+            Icon(
+                painter = leadingIcon,
+                contentDescription = "TextField Leading Icon",
+                modifier = Modifier.size(25.dp)
+            )
+        },
+        trailingIcon = { PasswordVisibility(passwordVisibility = passwordVisibility) },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            unfocusedBorderColor = Color(0xff222232),
+            backgroundColor = Color(0xff181829),
+            leadingIconColor = Color(0xff65656B)
+        ),
+        keyboardActions = onAction
+    )
+
+}
+
+@Composable
+fun PasswordVisibility(passwordVisibility: MutableState<Boolean>) {
+    val visible = passwordVisibility.value
+    IconButton(onClick = { passwordVisibility.value = !visible}) {
+//        Image(painter = painterResource(id = R.drawable.password), contentDescription = "")
+//        Icon(imageVector = Icons.Default.Visible, contentDescription = )
+    }
 }
