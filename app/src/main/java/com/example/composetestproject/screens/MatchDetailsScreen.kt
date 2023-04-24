c86913fb7b5f5a1f056c93cefc46df623d20fd09
aderemilekun <adelefatai@yahoo.com>
2023-04-24 13:28:17 +0100

    Line up section completed

--- app/src/main/java/com/example/composetestproject/screens/MatchDetailsScreen.kt
+++ app/src/main/java/com/example/composetestproject/screens/MatchDetailsScreen.kt
@@ -1,6 +1,7 @@
 package com.example.composetestproject.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -21,13 +22,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.composetestproject.MatchInfo
 import com.example.composetestproject.R
+import com.example.composetestproject.Team
 import com.example.composetestproject.components.ScoreCardComponent
 import com.example.composetestproject.components.SportyAppBar
 import com.example.composetestproject.components.TeamIcon
@@ -36,7 +40,7 @@ import com.example.composetestproject.navigation.AppScreens
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MatchDetailsScreen(navController: NavHostController) {
-    var infoSelected: MutableState<MatchInfo> = remember {
+    val infoSelected: MutableState<MatchInfo> = remember {
         mutableStateOf(MatchInfo.MatchDetails)
     }
 
@@ -76,60 +80,129 @@ fun MatchDetailsScreen(navController: NavHostController) {
                         infoSelected.value = MatchInfo.H2H
                     }
 
-//                    Text(text = "Line Up", color = Color.White, modifier = Modifier.padding(5.dp))
-//                    Text(text = "Line Up", color = Color.White, modifier = Modifier.padding(5.dp))
                 }
 
-                Column(modifier = Modifier.padding(vertical = 20.dp)) {
-                    MatchDetailsTile(data1 = "8", data2 = "12", name = "Shooting")
-                    MatchDetailsTile(data1 = "22", data2 = "29", name = "Attacks")
-                    MatchDetailsTile(data1 = "42", data2 = "58", name = "Possession")
-                    MatchDetailsTile(data1 = "3", data2 = "5", name = "Cards")
-                    MatchDetailsTile(data1 = "8", data2 = "7", name = "Corners")
-                }
-
-                Column(modifier = Modifier.padding(horizontal = 20.dp)) {
-                    Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp), horizontalArrangement = Arrangement.SpaceBetween) {
-                        Text(text = "Other Match", color = Color.White, fontSize = 18.sp, fontWeight = FontWeight.SemiBold)
-                        Text(text = "See all", color = Color(0xffC4C4C4), fontSize = 14.sp)
+                when (infoSelected.value) {
+                    MatchInfo.MatchDetails -> {
+                        MatchDetailsSection(navController)
                     }
-                    ScoreCardComponent(
-                        fTeamImage = R.drawable.astonvilla,
-                        sTeamImage = R.drawable.liverpool,
-                        fTeamName = "Aston Villa",
-                        sTeamName = "Liverpool",
-                        fTeamScore = "2",
-                        secTeamScore = "3",
-                        dur = "FT"
-                    ){
-                        navController.navigate(AppScreens.MatchDetailsScreen.name)
+                    MatchInfo.LineUp -> {
+                        LineUpSection(navController)
                     }
-                    ScoreCardComponent(
-                        fTeamImage = R.drawable.astonvilla,
-                        sTeamImage = R.drawable.liverpool,
-                        fTeamName = "Aston Villa",
-                        sTeamName = "Liverpool",
-                        fTeamScore = "2",
-                        secTeamScore = "3",
-                        dur = "FT"
-                    ){
-                        navController.navigate(AppScreens.MatchDetailsScreen.name)
-                    }
-                    ScoreCardComponent(
-                        fTeamImage = R.drawable.astonvilla,
-                        sTeamImage = R.drawable.liverpool,
-                        fTeamName = "Aston Villa",
-                        sTeamName = "Liverpool",
-                        fTeamScore = "2",
-                        secTeamScore = "3",
-                        dur = "FT"
-                    ){
-                        navController.navigate(AppScreens.MatchDetailsScreen.name)
+                    else -> {
+                        Box{}
                     }
                 }
             } 
         }
         
+    }
+}
+
+@Composable
+fun LineUpSection(navController: NavHostController) {
+    val teamSelected: MutableState<Team> = remember {
+        mutableStateOf(Team.Home)
+    }
+
+    Column(modifier = Modifier.padding(20.dp)) {
+        Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Formation", fontWeight = FontWeight.Bold, color = Color.White)
+            Spacer(modifier = Modifier.width(15.dp))
+            Text(text = "(4-2-3-1)", color = colorResource(id = R.color.text_color))
+        }
+       Row(modifier = Modifier.padding(vertical = 20.dp)) {
+           MatchDetailsIndicator(
+               "Arsenal",
+               height = 30.dp,
+               width = 100.dp,
+               color = if (teamSelected.value == Team.Home) {
+                   colorResource(id = R.color.select_sport_color)
+               } else {
+                   Color.Transparent
+               }
+           ) {
+               teamSelected.value = Team.Home
+           }
+           MatchDetailsIndicator(
+               "Aston Villa",
+               height = 33.dp,
+               width = 100.dp,
+               color = if (teamSelected.value == Team.Away) {
+                   colorResource(id = R.color.select_sport_color)
+               } else {
+                   Color.Transparent
+               }
+           ) {
+               teamSelected.value = Team.Away
+           }
+       }
+        Image(
+            painter = painterResource(id = R.drawable.fieldfootball),
+            contentDescription = "Team Line Up",
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+@Composable
+fun MatchDetailsSection(navController: NavHostController) {
+    Column(modifier = Modifier.padding(vertical = 20.dp)) {
+        MatchDetailsTile(data1 = "8", data2 = "12", name = "Shooting")
+        MatchDetailsTile(data1 = "22", data2 = "29", name = "Attacks")
+        MatchDetailsTile(data1 = "42", data2 = "58", name = "Possession")
+        MatchDetailsTile(data1 = "3", data2 = "5", name = "Cards")
+        MatchDetailsTile(data1 = "8", data2 = "7", name = "Corners")
+    }
+
+    Column(modifier = Modifier.padding(horizontal = 20.dp)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Other Match",
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+            Text(text = "See all", color = Color(0xffC4C4C4), fontSize = 14.sp)
+        }
+        ScoreCardComponent(
+            fTeamImage = R.drawable.astonvilla,
+            sTeamImage = R.drawable.liverpool,
+            fTeamName = "Aston Villa",
+            sTeamName = "Liverpool",
+            fTeamScore = "2",
+            secTeamScore = "3",
+            dur = "FT"
+        ) {
+            navController.navigate(AppScreens.MatchDetailsScreen.name)
+        }
+        ScoreCardComponent(
+            fTeamImage = R.drawable.astonvilla,
+            sTeamImage = R.drawable.liverpool,
+            fTeamName = "Aston Villa",
+            sTeamName = "Liverpool",
+            fTeamScore = "2",
+            secTeamScore = "3",
+            dur = "FT"
+        ) {
+            navController.navigate(AppScreens.MatchDetailsScreen.name)
+        }
+        ScoreCardComponent(
+            fTeamImage = R.drawable.astonvilla,
+            sTeamImage = R.drawable.liverpool,
+            fTeamName = "Aston Villa",
+            sTeamName = "Liverpool",
+            fTeamScore = "2",
+            secTeamScore = "3",
+            dur = "FT"
+        ) {
+            navController.navigate(AppScreens.MatchDetailsScreen.name)
+        }
     }
 }
 
@@ -152,11 +225,11 @@ fun MatchDetailsTile( data1: String, data2: String, name: String) {
 }
 
 @Composable
-fun MatchDetailsIndicator(name: String, color: Color, onTap: ()-> Unit) {
+fun MatchDetailsIndicator(name: String, color: Color, height: Dp = 50.dp,  width: Dp = 130.dp, onTap: ()-> Unit) {
     Surface(
         modifier = Modifier
-            .width(130.dp)
-            .height(50.dp)
+            .width(width)
+            .height(height)
             .clip(
                 RoundedCornerShape(
                     topStart = 25.dp,
